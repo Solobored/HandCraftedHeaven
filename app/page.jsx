b065f4390970e9f243c7929fcec4a25e8340454e
Solobored <josneiba10@gmail.com>
2025-07-07 15:39:00 -0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-cream-50">
+      <Header />
+      <main>
+        <Hero />
+        <FeaturedProducts />
+        <SellerSpotlight />
+      </main>
+      <Footer />
+    </div>
+  )
+}
